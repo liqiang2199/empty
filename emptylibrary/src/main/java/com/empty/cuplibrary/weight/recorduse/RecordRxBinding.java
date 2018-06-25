8057ab4684bc5984781d69195c82b660af7d621b
liqiang2199 <1094655541@qq.com>
2018-06-25 17:37:39 +0800

    RxBinding 的使用 合并控件

---
+++ emptylibrary/src/main/java/com/empty/cuplibrary/weight/recorduse/RecordRxBinding.java
@@ -0,0 +1,43 @@
+package com.empty.cuplibrary.weight.recorduse;
+
+import android.annotation.SuppressLint;
+import android.text.TextUtils;
+
+/**
+ * create by ${user} on ${date}
+ * 邮箱613239283@qq.com
+ * implementation 'com.jakewharton.rxbinding2:rxbinding:2.0.0'
+ * RxBinding 绑定 合并 两个控件
+ */
+public class RecordRxBinding {
+
+//    edit_1 = findViewById(R.id.edit_1);
+//    edit_2 = findViewById(R.id.edit_2);
+//    btn_edit = findViewById(R.id.btn_edit);
+//
+//    Observable<CharSequence> edit_One = RxTextView.textChanges(edit_1);
+//    Observable<CharSequence> edit_two = RxTextView.textChanges(edit_2);
+//
+//         Observable.combineLatest(edit_One, edit_two, new BiFunction<CharSequence, CharSequence, Object>() {
+//
+//        @Override
+//        public Object apply(CharSequence charSequence, CharSequence charSequence2) throws Exception {
+//
+//            return !TextUtils.isEmpty(charSequence.toString())
+//                    && !TextUtils.isEmpty(charSequence2.toString());
+//        }
+//    }).subscribe(new Consumer<Object>() {
+//        @SuppressLint("ResourceAsColor")
+//        @Override
+//        public void accept(Object o) throws Exception {
+//            if (o instanceof Boolean) {
+//                boolean b = (boolean) o;
+//                if (b) {
+//                    btn_edit.setBackgroundResource(R.color.colorPrimaryDark);
+//                }else {
+//                    btn_edit.setBackgroundResource(R.color.colorAccent);
+//                }
+//            }
+//        }
+//    });
+}
